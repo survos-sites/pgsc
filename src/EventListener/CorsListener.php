5be096588c41fc3c2caf7a6395de43e65d9459da
Amine G <amine@narodev.com>
2025-03-15 02:57:41 +0100

    add cors .

---
+++ src/EventListener/CorsListener.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\EventListener;
+
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+
+class CorsListener
+{
+    public function onKernelResponse(ResponseEvent $event)
+    {
+        $response = $event->getResponse();
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        $response->headers->set('Access-Control-Allow-Credentials', 'true');
+    }
+}
+
+?>
