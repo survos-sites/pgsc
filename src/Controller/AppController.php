8617e7b7eedcaa12598128e76719d854a3a2ec83
Tac Tacelosky <tacman@gmail.com>
2025-03-22 11:16:14 -0400

    add more data to  index page

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\Artist;
 use App\Entity\Location;
+use App\Repository\ArtistRepository;
+use App\Repository\LocationRepository;
 use Symfony\Bridge\Twig\Attribute\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,11 +16,41 @@ use Symfony\UX\Map\Point;
 
 final class AppController extends AbstractController
 {
+
+    public function __construct(
+        private LocationRepository $locationRepository,
+        private ArtistRepository $artistRepository,
+    )
+    {
+
+    }
     #[Route('/', name: 'app_homepage')]
     public function index(): Response
     {
+        $myMap = (new Map());
+            $myMap
+                // Explicitly set the center and zoom
+//                ->center($point)
+                ->zoom(16)
+                // Or automatically fit the bounds to the markers
+                ->fitBoundsToMarkers()
+
+            ;
+        foreach ($this->locationRepository->findAll() as $location) {
+            if ($location->getLat()) {
+                $point = new Point($location->getLat(), $location->getLng());
+                $myMap->addMarker(new Marker(
+                position: $point,
+                title: $location->getName(),
+            ));
+        }
+
+        }
         return $this->render('app/index.html.twig', [
             'controller_name' => 'AppController',
+            'artists' => $this->artistRepository->findAll(),
+            'locations' => $this->locationRepository->findAll(),
+            'myMap' => $myMap,
         ]);
     }
 
