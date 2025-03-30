6ee7e905c930e9b0543d52fb500c21281be5d445
Tac Tacelosky <tacman@gmail.com>
2025-03-30 07:44:04 -0400

    add tests, translations

---
+++ tests/Functional/MainTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Tests\Functional;
+
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Zenstruck\Browser\Test\HasBrowser;
+
+class MainTest extends KernelTestCase
+{
+    use HasBrowser;
+
+    public function testHomepage(): void
+    {
+        $kernel = self::bootKernel();
+        $this->assertSame('test', $kernel->getEnvironment());
+
+        $this->browser()
+            ->visit('/')
+            ->assertSuccessful()
+            ->saveSource('home.html')
+        ;
+
+
+    }
+}
