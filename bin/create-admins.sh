7ca4f5ada12b13e48dfa57db4b979e431fcda898
Tac Tacelosky <tacman@gmail.com>
2025-03-31 18:16:29 -0400

    add admin emails

---
+++ bin/create-admins.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+bin/console survos:user:create super@survos.com tt --roles ROLE_SUPER_ADMIN
+bin/console survos:user:create tac@survos.com tt  --roles ROLE_ADMIN
+bin/console survos:user:create yarenivillada@gmail.com batsi  --roles ROLE_ADMIN
+
+bin/console survos:user:create  tacman@gmail.com tt --roles ROLE_ALLOWED_TO_SWITCH --roles ROLE_ADMIN
