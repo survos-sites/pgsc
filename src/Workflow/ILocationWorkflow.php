2b905a09cfd661c4a08f8b7942b472bd425bd18c
Tac Tacelosky <tacman@gmail.com>
2025-04-13 12:38:31 -0400

    improve workflow metadata

--- src/Workflow/ILocationWorkflow.php
+++ src/Workflow/ILocationWorkflow.php
@@ -7,20 +7,32 @@ use Survos\WorkflowBundle\Attribute\Transition;
 
 interface ILocationWorkflow
 {
-	public const WORKFLOW_NAME = 'LocationWorkflow';
+    public const WORKFLOW_NAME = 'LocationWorkflow';
 
-	#[Place(initial: true)]
-	public const PLACE_NEW = 'new';
+    #[Place(initial: true, metadata: ['description' => "starting point, after persisted"])]
+    public const string PLACE_NEW = 'new';
 
-	#[Place]
-	public const PLACE_GEOCODED = 'geocoded';
+    #[Place]
+    public const string PLACE_GEOCODED = 'geocoded';
 
-	#[Place]
-	public const PLACE_APPROVED = 'approved';
+    #[Place(metadata: ['description' => "this place is now visible on the map and app"])]
+    public const PLACE_APPROVED = 'approved';
 
-	#[Transition(from: [self::PLACE_NEW], to: self::PLACE_GEOCODED)]
-	public const TRANSITION_GEOCODE = 'geocode';
+    #[Transition(
+        from: [self::PLACE_NEW],
+        to: self::PLACE_GEOCODED,
+        guard: 'subject.getAddress()',
+        metadata: [
+            'color' => 'green',
+            'arrow_color' => 'blue',
+            'description' => "geocode the address if it exists", 'hours' => '8-10PM', 'bg_color' => 'pink']
+    )]
+    public const TRANSITION_GEOCODE = 'geocode';
 
-	#[Transition(from: [self::PLACE_GEOCODED], to: self::PLACE_APPROVED)]
-	public const TRANSITION_APPROVE = 'approve';
+    #[Transition(
+        from: [self::PLACE_GEOCODED],
+        to: self::PLACE_APPROVED,
+        guard: 'is_granted("ROLE_ADMIN")',
+    )]
+    public const TRANSITION_APPROVE = 'approve';
 }
--- src/Workflow/LocationWorkflow.php
+++ src/Workflow/LocationWorkflow.php
@@ -7,68 +7,40 @@ use Survos\GeoapifyBundle\Service\GeoapifyService;
 use Survos\WorkflowBundle\Attribute\Workflow;
 use Symfony\Component\Workflow\Attribute\AsGuardListener;
 use Symfony\Component\Workflow\Attribute\AsTransitionListener;
+use Symfony\Component\Workflow\Event\CompletedEvent;
 use Symfony\Component\Workflow\Event\GuardEvent;
 use Symfony\Component\Workflow\Event\TransitionEvent;
 
-#[Workflow(supports: [Location::class], name: self::WORKFLOW_NAME)]
+#[Workflow(type: 'state_machine', supports: [Location::class], name: self::WORKFLOW_NAME)]
 class LocationWorkflow implements ILocationWorkflow
 {
     public const WORKFLOW_NAME = 'LocationWorkflow';
 
     public function __construct(
         private GeoapifyService $geoapifyService,
-    ) {
+    )
+    {
     }
 
-
-    #[AsGuardListener(self::WORKFLOW_NAME)]
-    public function onGuard(GuardEvent $event): void
+    private function getLocation(TransitionEvent $event): Location
     {
-        /** @var Location $location */
-        $location = $event->getSubject();
-
-        switch ($event->getTransition()->getName()) {
-        /*
-        e.g.
-        if ($event->getSubject()->cannotTransition()) {
-          $event->setBlocked(true, "reason");
-        }
-        App\Entity\Location
-        */
-            case self::TRANSITION_GEOCODE:
-                break;
-            case self::TRANSITION_APPROVE:
-                break;
-        }
+        /** @var Location */
+        return $event->getSubject();
     }
 
-
-    #[AsTransitionListener(self::WORKFLOW_NAME)]
-    public function onTransition(TransitionEvent $event): void
+    #[AsTransitionListener(self::WORKFLOW_NAME, self::TRANSITION_GEOCODE)]
+    public function onGeocode(TransitionEvent $event): void
     {
-        /** @var Location location */
-        $location = $event->getSubject();
-
-        switch ($event->getTransition()->getName()) {
-        /*
-        e.g.
-        if ($event->getSubject()->cannotTransition()) {
-          $event->setBlocked(true, "reason");
-        }
-        App\Entity\Location
-        */
-            case self::TRANSITION_GEOCODE:
-                if ($location->getAddress()) {
-                    $geo = $this->geoapifyService->lookup($location->getAddress())['features'][0]['properties'];
-                    if (!array_key_exists('lat', $geo)) {
-                        dd($geo);
-                    }
-                    $location->setLat($geo['lat'])
-                        ->setLng($geo['lon']);
-                }
-                break;
-            case self::TRANSITION_APPROVE:
-                break;
+        $location = $this->getLocation($event);
+        if ($location->getAddress()) {
+            $geo = $this->geoapifyService->lookup($location->getAddress())['features'][0]['properties'];
+            if (!array_key_exists('lat', $geo)) {
+                dd($geo);
+            }
+            $location->setLat($geo['lat'])
+                ->setLng($geo['lon']);
+        } else {
+            assert(false, "why didnt guard catch this? ");
         }
     }
 }
