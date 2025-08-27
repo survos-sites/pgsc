de15585d49b37c9b65a66422c652f941ce43daf4
Tac Tacelosky <tacman@gmail.com>
2025-08-27 11:54:49 -0400

    remove factory into tests

---
+++ tests/Factory/Factory/ArtistFactory.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Tests\Factory;
+
+// instead of namespace App\Tests\Factory;
+use App\Entity\Artist;
+use Zenstruck\Foundry\Persistence\PersistentProxyObjectFactory;
+
+/**
+ * @extends PersistentProxyObjectFactory<Artist>
+ */
+final class ArtistFactory extends PersistentProxyObjectFactory
+{
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
+     *
+     * @todo inject services if required
+     */
+    public function __construct()
+    {
+    }
+
+    public static function class(): string
+    {
+        return Artist::class;
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
+     *
+     * @todo add your default values here
+     */
+    protected function defaults(): array|callable
+    {
+        foreach (['facebook', 'twitter', 'instagram'] as $type) {
+            $social[] = "https://{$type}.com/".self::faker()->slug(1);
+        }
+        $name = self::faker()->name();
+
+        return [
+//            'socialMedia' => join("\n", $social),
+            'studioVisitable' => self::faker()->randomElement(Artist::STUDIO_VISITABLE),
+            //            'email' => self::faker()->email(),
+            //            'bio' => self::faker()->paragraph(3),
+            'birthYear' => self::faker()->numberBetween(1950, 2009),
+        ];
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
+     */
+    protected function initialize(): static
+    {
+        return $this
+            // ->afterInstantiate(function(Artist $artist): void {})
+        ;
+    }
+}
---
+++ tests/Factory/Factory/UserFactory.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Tests\Factory;
+
+use App\Entity\User;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Zenstruck\Foundry\Persistence\PersistentProxyObjectFactory;
+
+// src/Factory/UserFactory.php
+
+final class UserFactory extends PersistentProxyObjectFactory
+{
+    // the injected service should be nullable in order to be used in unit test, without container
+    public function __construct(
+        private ?UserPasswordHasherInterface $passwordHasher = null,
+    ) {
+        parent::__construct();
+    }
+
+    public static function class(): string
+    {
+        return User::class;
+    }
+
+    protected function defaults(): array
+    {
+        return [
+            'email' => self::faker()->unique()->safeEmail(),
+            'password' => '1234',
+            'roles' => ['ROLE_USER'],
+        ];
+    }
+
+    protected function initialize(): static
+    {
+        return $this
+            ->afterInstantiate(function (User $user) {
+                if (null !== $this->passwordHasher) {
+                    //                    dd($user->getPassword(), $user->getPlainPassword());
+                    $user->setPassword($this->passwordHasher->hashPassword($user, $user->getPlainPassword()));
+                }
+            })
+        ;
+    }
+}
